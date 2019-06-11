9d65987237f5e37411e7922ba11eb77e285450af
Ryolenyo <ryolenyo@gmail.com>
2019-06-11 15:59:37 +0800

    Hoe Behavior

---
+++ Assets/Scripts/HoeBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoeBehavior : MonoBehaviour
+{
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "BlueCube")
+        {
+            GameObject blueCube = GameObject.Find("BlueCube");
+            SpawnObjectFromTouch blueCubeVariable = blueCube.GetComponent<SpawnObjectFromTouch>();
+            blueCubeVariable.isTouchedByStick = true;
+        }
+    }
+}
---
+++ Assets/Scripts/SpawnObjectFromTouch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjectFromTouch : MonoBehaviour
+{
+    public bool isTouchedByStick;
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTouchedByStick = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTouchedByStick)
+        {
+            Instantiate(Sphere);
+            Sphere.transform.position = new Vector3(transform.position.x,transform.position.y+3,transform.position.z);
+        }
+    }
+}
