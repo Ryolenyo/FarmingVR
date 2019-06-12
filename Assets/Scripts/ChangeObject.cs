80be942bb2e7b2bbdbc4beb3968ff5b81b01a58e
nchp <nicha.lim@ku.th>
2019-06-12 16:54:40 +0900

    Initialize planting action

---
+++ Assets/Scripts/ChangeObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeObject : MonoBehaviour
+{
+    public bool isPlanted;
+    public GameObject Sphere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isPlanted = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPlanted)
+        {
+            Destroy(this);
+            Instantiate(Sphere, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+            isPlanted = false;
+        }
+    }
+}
---
+++ Assets/Seedling.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seedling : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "ground")
+        {
+            GameObject blueCube = collider.gameObject;
+            SpawnObjectFromTouch blueCubeVariable = blueCube.GetComponent<SpawnObjectFromTouch>();
+            blueCubeVariable.isTouchedByStick = true;
+        }
+    }
+}
