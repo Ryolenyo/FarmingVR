ce8342987a8e39a1b2df767ea8bd00e79fad6b36
Ryolenyo <ryolenyo@gmail.com>
2019-06-12 17:12:05 +0800

    Add Filling and Watering

---
+++ Assets/Scripts/PlantBehavior.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlantBehavior : MonoBehaviour
+{
+
+    public bool isReady = false;
+    public float currentWater = 0;
+    public float maxWater = 50;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentWater > maxWater)
+        {
+            isReady = true;
+        }
+
+        if (isReady)
+        {
+            if (transform.position.y > 1)
+            {
+                isReady = false;
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + 0.01f, transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/WaterCanBehavior.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterCanBehavior : MonoBehaviour
+{
+    public float currentVolume = 0;
+    public float maxVolume = 500;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnTriggerStay(Collider collider)
+    {
+        //Filling water can
+        if (collider.gameObject.name == "Well")
+        {
+            if (currentVolume < maxVolume)
+            {
+                currentVolume = currentVolume + 1;
+            }
+            Debug.Log("filling : "+currentVolume);
+        }
+
+        //Watering plant
+        if (collider.gameObject.tag == "Sapling")
+        {
+            //Call plant variable
+            GameObject plant = collider.gameObject;
+            PlantBehavior plantVariable = plant.GetComponent<PlantBehavior>();
+
+            if (currentVolume > 0)
+            {
+                currentVolume = currentVolume - 1;
+                plantVariable.currentWater = plantVariable.currentWater + 1;
+            }
+            Debug.Log("watering: "+currentVolume);
+        }
+    }
+}
