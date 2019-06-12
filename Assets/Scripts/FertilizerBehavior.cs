8eccc2712074c6a8703d720d26e35c27d4a4de4a
iidleme <rawissara.r@ku.th>
2019-06-12 16:56:05 +0900

    Add collider

--- Assets/Scripts/FertilizerBehavior.cs
+++ Assets/Scripts/FertilizerBehavior.cs
@@ -5,15 +5,22 @@ using UnityEngine;
 public class FertilizerBehavior : MonoBehaviour
 {
     public int max = 10;
+    public bool used;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        used = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(used)
+        {
+            max--;
+
+            used = false;
+        }
     }
 }
