d23719a4814ed674f30656718e97f16d966fb11a
iidleme <rawissara.r@ku.th>
2019-06-12 16:09:24 +0900

    Add fertilizer temporary model & script for fertilized plant

---
+++ Assets/Scripts/Fertilized.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fertilized : MonoBehaviour
+{
+    public int plantValue;
+    public bool fertilized;
+    private bool done;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        fertilized = false;
+        done = false;
+        plantValue = Random.Range(5, 101);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(fertilized && !done)
+        {
+            plantValue += 10 / 100;
+            done = true;
+        }
+        Debug.Log("value: " + plantValue);
+    }
+}
