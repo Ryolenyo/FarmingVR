1c46607fe298bfd63f5a3f534c6f39e1d5c76b35
Ryolenyo <ryolenyo@gmail.com>
2019-06-13 13:07:53 +0800

    Growing up and Harvest

---
+++ Assets/Scripts/PlantGrowBehavior.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlantGrowBehavior : MonoBehaviour
+{
+    public GameObject nextObject; // Next state of plant
+    public float targetTime;
+    public bool isTimerRun = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isTimerRun)
+        {
+            timer(targetTime);    
+        }
+    }
+
+    void timer(float time)
+    {
+
+        if (targetTime <= 0.0f)
+        {
+            isTimerRun = false;
+            changeState();
+        }
+        else
+        {
+            targetTime -= Time.deltaTime;
+        }
+
+    }
+
+    void changeState()
+    {
+        //Debug.Log("CHANGING...");
+ 
+        targetTime = 3.0f;
+        GameObject newObject;
+        newObject = (GameObject)EditorUtility.InstantiatePrefab(nextObject);
+        newObject.transform.position = transform.position;
+        newObject.transform.rotation = transform.rotation;
+
+        Destroy(gameObject);
+
+    }
+
+    //Check harvesting and stop growing up
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "AllGround") // Collider of [Empty in Ground] that is set the tag to "AllGround"
+        {
+            //Debug.Log("STOP GROWING UP");
+            isTimerRun = false;
+        }
+    }
+}
