5c2701f7cb7cef527c08ff6aea83e6470ae80da9
nchp <nicha.lim@ku.th>
2019-06-14 11:16:54 +0900

    Try syncing actual timer with appeared timer.

---
+++ Assets/Scripts/SceneLoadinator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadinator : MonoBehaviour
+{
+    [SerializeField]
+    private float timer;
+
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElapsed;
+
+    public bool timeOver;
+
+    void Start()
+    {
+        timeOver = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+
+        if(timeElapsed > timer)
+        {
+            SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Timer : MonoBehaviour
+{
+
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    [SerializeField] Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startingTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentTime -= 1 * Time.deltaTime;
+        timerText.text = currentTime.ToString("0");
+
+        if(currentTime <= 0)
+        {
+            currentTime = 0;
+        }
+    }
+}
