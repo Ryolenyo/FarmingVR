53c1cc15cb2023ab6705f4d12fc760df03a343d8
iidleme <rawissara.r@ku.th>
2019-06-25 16:03:23 +0900

    Add objective type

--- Assets/Scripts/ObjectiveGiver.cs
+++ Assets/Scripts/ObjectiveGiver.cs
@@ -5,25 +5,28 @@ using UnityEngine;
 public class ObjectiveGiver : MonoBehaviour
 {
     public Objective objective;
-    public string[] objectiveTitle =
+    private string[] objectiveDescription =
     {
         "Collect 10 crops during golden time",
         "Plant only monocarpic crops",
         "Get 1000 score without using fertilizer",
         "Get 500 within the first 2 minutes of the game",
-        "Handle moles without using trapper",
+        "Handle moles without using trapper or net",
+    };
+    private ObjectiveType[] objectiveType =
+    {
+        ObjectiveType.Collecting,
+        ObjectiveType.Restriction,
+        ObjectiveType.Restriction,
+        ObjectiveType.Restriction,
+        ObjectiveType.Restriction
     };
 
     // Start is called before the first frame update
     void Start()
     {
         int num = Random.Range(0, 5);
-        objective.title = objectiveTitle[num];
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        objective.description = objectiveDescription[num];
+        objective.type.objectiveType = objectiveType[num];
     }
 }
---
+++ Assets/Scripts/ObjectiveGoal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class ObjectiveGoal
+{
+    public ObjectiveType objectiveType;
+}
+
+public enum ObjectiveType
+{
+    Collecting,
+    Restriction
+}
