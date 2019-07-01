44a00ec47c0002e6ad9b5a09a546836250bc7023
iidleme <rawissara.r@ku.th>
2019-07-01 14:28:57 +0900

    Edit prefabs

--- Assets/Scripts/ObjectiveGiver.cs
+++ Assets/Scripts/ObjectiveGiver.cs
@@ -10,8 +10,15 @@ public class ObjectiveGiver : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        objective.description = "collect 10 gold products";
+        /*objective.description = "collect 10 gold products";
         objective.type = ObjectiveType.Collecting;
+
+        requiredObject = new string[] {"Gold"};
+
+        objective.Init(10, requiredObject);*/
+
+		objective.description = "Sell 10 gold fruit in the first minute of the game";
+        objective.type = ObjectiveType.TimeLimit;
 
         requiredObject = new string[] {"Gold"};
 
