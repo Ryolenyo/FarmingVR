0cf115290279b9816712adb47797d951d643ed68
iidleme <rawissara.r@ku.th>
2019-07-01 17:37:29 +0900

    Update Objective system

--- Assets/Scripts/ObjectiveGoal.cs
+++ Assets/Scripts/ObjectiveGoal.cs
@@ -17,6 +17,10 @@ public class ObjectiveGoal
     {
         requiredAmount = amount;
         requiredObject = name;
+
+		GameObject basket = GameObject.Find("Basket").transform.Find("basketVoid").gameObject;
+		BasketBehavior basketVariable = basket.GetComponent<BasketBehavior>();
+		basketVariable.objectiveChecker = true;
     }
 
     public void CollectingComplete(string sellingObject)
@@ -46,6 +50,10 @@ public class ObjectiveGoal
 		requiredAmount = amount;
         requiredObject = name;
 		limit = time;
+
+		GameObject basket = GameObject.Find("Basket").transform.Find("basketVoid").gameObject;
+		BasketBehavior basketVariable = basket.GetComponent<BasketBehavior>();
+		basketVariable.objectiveChecker = true;
     }
 
 	public void TimeLimitComplete(string sellingObject)
