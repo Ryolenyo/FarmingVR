9264740ad96a3f03d3a881a56ea74137707ab4d3
iidleme <rawissara.r@ku.th>
2019-07-04 17:51:23 +0900

    Update objective system

--- Assets/Scripts/ObjectiveGiver.cs
+++ Assets/Scripts/ObjectiveGiver.cs
@@ -4,18 +4,40 @@ using UnityEngine;
 
 public class ObjectiveGiver : MonoBehaviour
 {
+	public int n;
+	public ObjectiveList list;
+
     public Objective objective;
     public string[] requiredObject;
+	public int requiredAmount;
+	public int time;
 
     // Start is called before the first frame update
     void Start()
     {
-        objective.description = "collect 10 gold products";
+		n = Random.Range(0, list.size);
+		
+		objective.description = list.description[n];
+        objective.type = list.type[n];
+        //requiredObject[0] = list.requiredObject[n,0];
+		/*for (int i = 0; i < list.requiredObject.GetLength(1); i++)
+		{
+			if (list.requiredObject[n, i] != null)
+			{
+				requiredObject[0] = list.requiredObject[n, 0];
+			}
+		}*/
+		requiredAmount = list.requiredAmount[n];
+		time = list.time[n];
+        
+		objective.Init(requiredAmount, requiredObject, time);
+
+        /*objective.description = "collect 10 gold products";
         objective.type = ObjectiveType.Collecting;
 
         requiredObject = new string[] {"Gold"};
 
-        objective.Init(10, requiredObject, 0);
+        objective.Init(10, requiredObject, 0);*/
 
 		/*objective.description = "Sell 10 gold fruits in the first minute of the game";
         objective.type = ObjectiveType.TimeLimit;
---
+++ Assets/Scripts/ObjectiveList.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public class ObjectiveList
+{
+	public int size = 3;
+	
+	public string[] description = new string[] {
+		"Collect 10 golden products",
+		"Sell 10 golden products in the first minute of the game",
+		"Handle the mole without using trapper or net"
+	};
+
+	public ObjectiveType[] type = new ObjectiveType[] {
+		ObjectiveType.Collecting,
+		ObjectiveType.TimeLimit,
+		ObjectiveType.Restriction
+	};
+
+	public string[,] requiredObject = new string[,] {
+		{"Gold"},
+		{"Gold"},
+		{""}
+	};
+
+	public int[] requiredAmount = new int[] {
+		10,
+		10,
+		0
+	};
+
+	public int[] time = new int[] {
+		0,
+		60,
+		0
+	};
+}
