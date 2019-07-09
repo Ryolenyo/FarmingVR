99692622a1942ee855de3b67d602a8083d2c4f30
Ryolenyo <ryolenyo@gmail.com>
2019-07-09 16:45:13 +0900

    Update

--- Assets/Scripts/NetworkConnectionManager.cs
+++ Assets/Scripts/NetworkConnectionManager.cs
@@ -1,18 +1,41 @@
-﻿using System.Collections;
+﻿using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class NetworkConnectionManager : MonoBehaviour
+public class NetworkConnectionManager : MonoBehaviourPunCallbacks
 {
+    public Button BtnConnectMaster;
+    public Button BtnConnectRoom;
+
+    public bool TriesToConnectToMaster;
+    public bool TriesToConnectToRoom;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        TriesToConnectToMaster = false;
+        TriesToConnectToRoom = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        BtnConnectMaster.gameObject.SetActive(!PhotonNetwork.IsConnected && !TriesToConnectToMaster);
+        BtnConnectRoom.gameObject.SetActive(PhotonNetwork.IsConnected && !TriesToConnectToMaster && !TriesToConnectToRoom);
     }
+
+    public void onClickConnectToMaster()
+    {
+        PhotonNetwork.OfflineMode = false;
+        PhotonNetwork.NickName = "PlayerName";
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.GameVersion = "v1";
+
+        TriesToConnectToMaster = true;
+
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
 }
