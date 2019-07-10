77c7326cdf9578fde80dad7c9085326827041924
iidleme <rawissara.r@ku.th>
2019-07-10 16:39:35 +0900

    Reset mole's transform

--- Assets/Scripts/Mole.cs
+++ Assets/Scripts/Mole.cs
@@ -11,7 +11,7 @@ public class Mole : MonoBehaviour
 
     //public float eatTime = 5;
     public float currentTime = 0;
-	public float upTime = 6.07;
+	public float upTime = 6.07f;
     public float comebackTime = 15;
 
 	public GameObject[] animation;
@@ -47,6 +47,39 @@ public class Mole : MonoBehaviour
 					isGoUp = false;
 					isGoDown = true;
 					currentTime = 0;
+				}
+			}
+			else if (isGoDown)
+			{
+				animation[0].SetActive(false);
+				animation[1].SetActive(true);
+				animation[2].SetActive(false);
+
+				if (animation[1].transform.localPosition.y > -0.3f)
+				{
+					animation[1].transform.position = new Vector3(animation[1].transform.position.x, animation[1].transform.position.y - 0.005f, animation[1].transform.position.z);
+				}
+				else
+				{
+					animation[1].SetActive(false);
+					//animation[1].transform.localPosition.y = -0.07f;
+
+					if (trapper != null)
+					{
+						animation[0].SetActive(true);
+						animation[2].SetActive(false);
+
+						transform.position = new Vector3(trapper.transform.position.x, transform.position.y, trapper.transform.position.z);
+                        transform.rotation = Quaternion.identity;
+					}
+					else
+					{
+						randomGround = Random.Range(0, 17);
+						transform.localPosition = new Vector3(target[randomGround].transform.position.x + 0.25f , transform.position.y, target[randomGround].transform.position.z - 0.25f);
+                        transform.rotation = Quaternion.identity;
+					}
+
+					isGoUp = true;
 				}
 			}
 		}
