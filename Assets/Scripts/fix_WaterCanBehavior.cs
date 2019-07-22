2c745c28252cd12520a1a68b9ffd5c675d68ade1
Ryolenyo <ryolenyo@gmail.com>
2019-07-22 18:03:55 +0900

    Edit

--- Assets/Scripts/fix_WaterCanBehavior.cs
+++ Assets/Scripts/fix_WaterCanBehavior.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class fix_WaterCanBehavior : MonoBehaviour
 {
+    public GameObject FillingSfx;
+    public GameObject WateringSfx;
+
+    public bool isFill = false;
+    public bool isWat = false;
 
     public float currentVolume = 0;
     public float maxVolume = 1000;
@@ -11,12 +16,27 @@ public class fix_WaterCanBehavior : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isFill && !isWat)
+        {
+            FillingSfx.SetActive(true);
+            WateringSfx.SetActive(false);
+        }
+        else if (!isFill && isWat)
+        {
+            FillingSfx.SetActive(false);
+            WateringSfx.SetActive(true);
+        }
+        else
+        {
+            FillingSfx.SetActive(false);
+            WateringSfx.SetActive(false);
+        }
     }
 
     void OnTriggerStay(Collider collider)
@@ -29,8 +49,16 @@ public class fix_WaterCanBehavior : MonoBehaviour
             if (currentVolume < maxVolume)
             {
                 currentVolume = currentVolume + 5;
+                isFill = true;
+                isWat = false;
+            }
+            else
+            {
+                isFill = false;
+                isWat = false;
             }
             //Debug.Log("filling : " + currentVolume);
+
         }
 
         //Watering ground
@@ -45,7 +73,7 @@ public class fix_WaterCanBehavior : MonoBehaviour
                     GroundBehavior groundVariable = colObject.GetComponent<GroundBehavior>();
                     groundVariable.volume = groundVariable.volume + 1;
 
-                    if(groundVariable.volume > groundVariable.maxVolume)
+                    if (groundVariable.volume > groundVariable.maxVolume)
                     {
                         groundVariable.isWatered = true;
                     }
@@ -56,7 +84,21 @@ public class fix_WaterCanBehavior : MonoBehaviour
                     currentVolume = currentVolume - 1;
                     //Debug.Log("spilling: " + currentVolume);
                 }
+                isFill = false;
+                isWat = true;
             }
+            else
+            {
+                isFill = false;
+                isWat = false;
+            }
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "DigGround" || other.tag == "Planted" || other.tag == "AllGround")
+        {
+            isWat = false;
         }
     }
 }
