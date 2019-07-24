670f68e9bfafde3a92e0092efd3f3417c75f597f
iidleme <rawissara.r@ku.th>
2019-07-24 14:49:46 +0900

    Update weed's script
    - Destroyed when pulled out

--- Assets/Scripts/WeedBehavior.cs
+++ Assets/Scripts/WeedBehavior.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class WeedBehavior : MonoBehaviour
 {
+	private bool isPulled = false;
+	private float timeTarget = 10;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,8 +16,19 @@ public class WeedBehavior : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		if (isPulled)
+		{
+			if (timeTarget > 0)
+			{
+				timeTarget -= Time.deltaTime;
+			}
+			else
+			{
+				Destroy(gameObject);
+			}
+		}
     }
+
     private void OnTriggerExit(Collider other)
     {
         if (other.tag == "AllGround")
@@ -23,6 +37,8 @@ public class WeedBehavior : MonoBehaviour
             Collider weedCol = GetComponent<Collider>();
             weed.useGravity = true;
             weedCol.isTrigger = false;
+			
+			isPulled = true;
         }
     }
 }
